2ae2a5b1b27e02b64e27bf6924d0fbadd03f1b02
accountgithub2003 <95201020+duongnhatthien@users.noreply.github.com>
2024-11-05 02:49:31 +0700

    Print result

--- chuongtrinh.php
+++ chuongtrinh.php
@@ -2,11 +2,11 @@
 // Định nghĩa tên các hàm
 function add($a, $b)
 {
-    echo 'a + b = '.$a+$b;
+    echo 'a + b = ' . $a + $b;
 }
 function subtract($a, $b)
 {
-    echo '<br>'.'a - b = '.$a-$b;
+    echo '<br>' . 'a - b = ' . $a - $b;
 }
 function multiply($a, $b)
 {
@@ -14,4 +14,5 @@ function multiply($a, $b)
 function divide($a, $b)
 {
 }
+echo "Kết quả là phép cộng là: ", add(1, 3);
 ?>
