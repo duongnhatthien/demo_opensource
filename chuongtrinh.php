6814571ed824afc65d07200bfa918036771384c7
accountgithub2003 <95201020+duongnhatthien@users.noreply.github.com>
2024-11-05 02:39:27 +0700

    add mul and div function

--- chuongtrinh.php
+++ chuongtrinh.php
@@ -6,10 +6,14 @@ function add($a, $b)
 function subtract($a, $b)
 {
 }
-function multiply($a, $b)
-{
+function multiply($a, $b) {
+    return $a * $b;
 }
-function divide($a, $b)
-{
+
+function divide($a, $b) {
+    if ($b == 0) {
+        return "Cannot divide by zero!";
+    }
+    return $a / $b;
 }
 ?>
